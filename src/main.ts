34f9afcac7bd63b6eebd93b4523f177981a47cda
Khoa <khoanguyen.1881@gmail.com>
2025-03-25 20:45:29 +0700

    validate and custumize dto with pipe
    Cần khai báo global pipe ở main.ts trước
    đọc tài liệu để biết thêm.(https://docs.nestjs.com/techniques/validation). Để custom message vd: sang tiếng việt==> đọc tài liệu của class-validator hoặc search.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe()); //- sử dụng pipe để validate dữ liệu trước khi vào controller
+
   await app.listen(configService.get<string>('PORT') ?? 3000);
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsEmail({}, { message: 'Email không hợp lệ' })
+  @IsNotEmpty({ message: 'Email không được để trống' })
   email: string;
+
+  @IsNotEmpty({ message: 'Mật khẩu không được để trống' })
   password: string;
+
   name: string;
   phone: string;
   age: number;
