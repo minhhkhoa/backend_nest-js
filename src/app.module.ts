dec1f1f442975d2d19b53e66607d87754c5e42e4
Khoa <khoanguyen.1881@gmail.com>
2025-03-25 13:49:31 +0700

    Create module users.
    có thể dùng cli để tạo nhanh: nest g resource users --no-spec. Cờ --no-spec có mục đích là khi tạo module(tính năng) sẽ không tạo kèm file .spec(file test).

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
+      // eslint-disable-next-line @typescript-eslint/require-await
+      useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGO_URL'),
       }),
       inject: [ConfigService],
@@ -16,6 +18,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ConfigModule.forRoot({
       isGlobal: true, //---> This is a global configuration
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
